0577cfe380663a4d21886d2b140d93707683019c
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-06-25 21:37:07 +0530

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,16 +5,12 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// FlatCompat ka istemaal purani ESLint settings ko new Flat ESLint ke saath use karne ke liye
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-export default [
+const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off", // any error band
-    },
-  },
 ];
+
+export default eslintConfig;
