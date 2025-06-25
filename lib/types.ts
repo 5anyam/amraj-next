3571dc6708d0c6ceac8bd63861a53ff4647334e5
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-06-25 23:11:47 +0530

    Initial commit

--- lib/types.ts
+++ lib/types.ts
@@ -1,11 +1,19 @@
 // lib/types.ts
-export interface Product {
+export interface ImageData {
+    src: string;
+  }
+  
+  export interface Attribute {
+    option: string;
+  }
+  
+  export interface Product {
     id: number;
     name: string;
     price: string;
     description?: string;
     short_description?: string;
-    images?: { src: string }[];
-    attributes?: { option: string }[];
+    images?: ImageData[];
+    attributes?: Attribute[];
   }
   
